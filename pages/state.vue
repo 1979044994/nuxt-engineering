2ff75a172321f80e03bf5aa7bd055706d40f8231
linzhicai <linzhicai@dobest.com>
2024-09-18 10:46:45 +0800

    fix

--- pages/state.vue
+++ pages/state.vue
@@ -1,4 +1,31 @@
 <template>
+  <div class="flex flex-col">
+    <div class="mt-2"> {{ count }}</div>
+    <div class="mt-2">{{ counterRef }}</div>
+    <Button class="w-20" @click="handleAdd">+</Button>
+    <Button class="w-20" @click="handleSub">-</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+let count = useState("counter", () => Math.round(Math.random() * 1000));
+const counterRef = ref(Math.round(Math.random() * 1000));
+const handleAdd = () => {
+  count.value++;
+  counterRef.value++;
+};
+const handleSub = () => {
+  count.value--;
+  counterRef.value--;
+};
+</script>
+
+<style lang="scss" scoped>
+.asd {
+  flex-direction: column;
+}
+</style>
+<template>
   <div>
     <h2>用户信息</h2>
     <p>用户名：{{ username }}</p>
