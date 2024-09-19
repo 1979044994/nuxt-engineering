4e46668bd9e5db22900f7cdb6dc73cb5e38ad4c8
linzhicai <linzhicai@dobest.com>
2024-09-19 14:34:08 +0800

    fix

---
+++ pages/setState.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col">
+    <div class="mt-2"> {{ count }}</div>
+    <div class="mt-2">{{ counterRef }}</div>
+    <Button class="w-20" @click="handleAdd">+</Button>
+    <Button class="w-20" @click="handleSub">-</Button>
+    <Button class="w-20" @click="handleSetCookie">setCookie</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+let count = useState("counter", () => Math.round(Math.random() * 1000));
+const counterRef = ref(Math.round(Math.random() * 1000));
+const handleAdd = () => {
+  count.value++;
+  counterRef.value++;
+};
+const handleSub = () => {
+  count.value--;
+  counterRef.value--;
+};
+const test = useCookie('test', {
+  default: () => (0),
+  watch: true
+})
+const handleSetCookie = () => {
+  console.log(test.value)
+  test.value++;
+
+}
+</script>
+
+<style lang="scss" scoped>
+.asd {
+  flex-direction: column;
+}
+</style>
--- pages/state.vue
+++ pages/state.vue
@@ -1,41 +1,4 @@
 <template>
-  <div class="flex flex-col">
-    <div class="mt-2"> {{ count }}</div>
-    <div class="mt-2">{{ counterRef }}</div>
-    <Button class="w-20" @click="handleAdd">+</Button>
-    <Button class="w-20" @click="handleSub">-</Button>
-    <Button class="w-20" @click="handleSetCookie">setCookie</Button>
-  </div>
-</template>
-
-<script setup lang="ts">
-let count = useState("counter", () => Math.round(Math.random() * 1000));
-const counterRef = ref(Math.round(Math.random() * 1000));
-const handleAdd = () => {
-  count.value++;
-  counterRef.value++;
-};
-const handleSub = () => {
-  count.value--;
-  counterRef.value--;
-};
-const test = useCookie('test', {
-  default: () => (0),
-  watch: true
-})
-const handleSetCookie = () => {
-  console.log(test.value)
-  test.value++;
-
-}
-</script>
-
-<style lang="scss" scoped>
-.asd {
-  flex-direction: column;
-}
-</style>
-<template>
   <div>
     <h2>用户信息</h2>
     <p>用户名：{{ username }}</p>
